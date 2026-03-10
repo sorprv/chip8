e20ca869634d29e95bb3fd40856610cca59e1bf8
uan.tukfatullin <tytyta805>
2026-03-10 10:29:32 +0300

    Дополнили файл main.c

--- source/main.c
+++ source/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+
+#define CHIP8_MEMORY_SIZE	4096
+#define CHIP8_DISPLAY_WIDTH	64
+#define CHIP8_DISPLAY_HEIGHT	32
+#define V_REGISTER_COUNT	16
+#define STACK_DEPTH		16
+#define CHIP8_KEY_COUNT		16
+
+
 int main() {
+	typedef struct Chip8State{
+		int a;
+		int b;
+	};
 	printf("Hello, World!");
 	return 0;
 }
